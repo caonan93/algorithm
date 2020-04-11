469552f4e8bc2e841f477c4f5309af2c10c808b8
hulkcao <hulkcao@tencent.com>
2020-04-11 15:38:55 +0800

    update 56 mergeintervals

--- leetcode/56.MergeIntervals.cpp
+++ leetcode/56.MergeIntervals.cpp
@@ -9,29 +9,66 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-//https://leetcode.com/problems/merge-intervals/
+// https://leetcode.com/problems/merge-intervals/
 class Solution {
 public:
 
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        for(auto& arr:intervals){
-			if(arr[0]>arr[1]) std::swap(arr[0],arr[1]);
-		}
-		std::sort(intervals.begin(),intervals.end(),[](vector<int> a,vector<int> b){return a[0]<b[0];});
-		for(auto& arr:intervals){
-			printf("[%d,%d] ",arr[0],arr[1]);
-		}
-		cout<<endl;
-		return vector<vector<int>>();
+        for(auto& arr:intervals) {
+            if(arr[0]>arr[1]) std::swap(arr[0],arr[1]);
+        }
+        std::sort(intervals.begin(),intervals.end(),[](vector<int> a,vector<int> b) {
+            return a[0]<b[0];
+        });
+
+        vector<vector<int>> result;
+        for(int i=0; i<intervals.size();)
+        {
+            vector<int> tupe = intervals[i];
+            if(i+1>=intervals.size()) {
+                result.push_back(tupe);
+                break;
+            }
+            vector<int> next_tupe = intervals[i+1];
+            while(1)
+            {
+                if(tupe[1]<next_tupe[0]) {
+                    result.push_back(tupe);
+                    i++;
+                    break;
+                }
+                else {
+                    if(tupe[1]<next_tupe[1]) tupe = vector<int> {tupe[0],next_tupe[1]};
+                    else tupe = vector<int> {tupe[0],tupe[1]};
+                    i++;
+                    if(i+1 >=intervals.size()) {
+                        result.push_back(tupe);
+                        return result;
+                    }
+                    next_tupe = intervals[i+1];
+                }
+            }
+        }
+        return result;
     }
 };
 
-
+void test(vector<vector<int>> arr)
+{
+    Solution s;
+    auto intervals =s.merge(arr);
+    for(auto& arr:intervals) {
+        printf("[%d,%d] ",arr[0],arr[1]);
+    }
+    printf("\n");
+}
 int main()
 {
-	Solution s;
-	vector<vector<int>> init_arr{{1,3},{2,6},{8,10},{3,9}};
-	s.merge(init_arr);
+    test({{1,3},{4,6},{8,10},{3,9}});
+    test({{1,3},{2,6},{8,10},{15,18}});
+    test({{1,4},{4,5}});
+    test({{1,100}});
+    test({});
     return 0;
 }
 
